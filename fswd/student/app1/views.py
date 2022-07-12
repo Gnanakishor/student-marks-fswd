80eabd02159860011518ddda24d8a6bb7944013a
Gnanakishor <101774717+Gnanakishor@users.noreply.github.com>
2022-07-12 19:51:04 +0530

    uploaded

---
+++ fswd/student/app1/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def signup(request):
+    return render(request,'signup.html')	
+
+
+
+def login(request):
+    return render(request,'login.html')
+
+
+
